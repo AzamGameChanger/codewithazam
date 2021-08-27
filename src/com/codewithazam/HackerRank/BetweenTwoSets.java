2990377591c39b15e934473a9b381025a7c237c7
Azamkhuja Ibragimov <ibragimov.azamkhuja.2020@gmail.com>
2021-08-26 21:28:50 -0400

    Asia made some changes to the project

--- src/com/codewithazam/HackerRank/BetweenTwoSets.java
+++ src/com/codewithazam/HackerRank/BetweenTwoSets.java
@@ -7,19 +7,9 @@ import java.util.List;
 public class BetweenTwoSets {
     public static void main(String[] args) {
 
-        List<Integer> list1 = Arrays.asList(2,6);
-        List<Integer> list2 = Arrays.asList(24,36);
-int min =6;
-int max=24;
-for (int i=min;i<=max;i++)
-{
-    
-}
+        System.out.println("This is from codewithazam user.");
+        System.out.println("Making some changes to the project.");
     }
 
-    public static int getTotalX(List<Integer> a, List<Integer> b) {
-        // Write your code here
-        int x = 0;
-        return x;
-    }
+
 }
