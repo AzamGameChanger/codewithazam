14b4a68294b9eab835da678f159139f7b29f8e05
Azamkhuja Ibragimov <ibragimov.azamkhuja.2020@gmail.com>
2021-08-29 12:58:22 -0400

    American development team

---
+++ src/com/codewithazam/test3.java
@@ -0,0 +1,9 @@
+package com.codewithazam;
+
+public class test3 {
+    public static void main(String[] args) {
+        System.out.println("American development team");
+
+        System.out.println("Pulled the developed branch to make changes");
+    }
+}
---
+++ src/com/codewithazam/test4.java
@@ -0,0 +1,12 @@
+package com.codewithazam;
+
+public class test4 {
+    public void getData(){
+        System.out.println("Verifying the pop up window in the home page");
+    }
+
+    public static void main(String[] args) {
+        test4 f = new test4();
+        f.getData();
+    }
+}
