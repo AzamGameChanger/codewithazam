27275a01527a3adb559bd51b3373aa1c896898c1
Azamkhuja Ibragimov <ibragimov.azamkhuja.2020@gmail.com>
2021-08-29 12:51:22 -0400

    Asian development team

---
+++ src/com/codewithazam/Test1.java
@@ -0,0 +1,11 @@
+package com.codewithazam;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Asian development team");
+
+        System.out.println("Created a new branch develop");
+
+
+    }
+}
---
+++ src/com/codewithazam/Test2.java
@@ -0,0 +1,14 @@
+package com.codewithazam;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+    @Test
+    public void loginRequest(){
+        System.out.println("Login is verified successfully");
+    }
+
+    public void breakDown(){
+        System.out.println("driver closed");
+    }
+}
